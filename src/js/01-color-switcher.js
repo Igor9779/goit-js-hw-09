16e3feb5bf2cccc1d2b922211d9e19d4b9ffe53b
Igor9779 <darthhoit97@gmail.com>
2023-07-18 18:54:56 +0300

    color-switcher-complete

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const refs = {
+    start: document.querySelector('[data-start]'),
+    stop: document.querySelector('[data-stop]'),
+    bodyColor: document.querySelector('body'),
+};
+
+refs.stop.disabled = true;
+
+const onClickStartChangeColor = () => {
+    timerId = setInterval(() => {
+        refs.bodyColor.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    changeState();
+};
+
+const onClickStop = () => {
+    clearInterval(timerId);
+    changeState();
+};
+
+function changeState() {
+    if (!refs.start.disabled) {
+        refs.start.disabled = true;
+        refs.stop.disabled = false;
+    } else {
+        refs.stop.disabled = true;
+        refs.start.disabled = false;
+    };
+};
+
+refs.start.addEventListener('click', onClickStartChangeColor);
+refs.stop.addEventListener('click', onClickStop);
