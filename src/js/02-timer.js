cf1ee5980dd769eacf4f9a0d31fbbd770c33f07c
Igor9779 <darthhoit97@gmail.com>
2023-07-19 16:29:44 +0300

    countdown-timer-1

---
+++ src/js/02-timer.js
@@ -0,0 +1,63 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+let timerId = null;
+
+refs = {
+    input: document.querySelector('#datetime-picker'),
+    button: document.querySelector('[data-start]'),
+    days: document.querySelector('[data-days]'),
+    hours: document.querySelector('[data-hours]'),
+    minutes: document.querySelector('[data-minutes]'),
+    seconds: document.querySelector('[data-seconds]'),
+};
+
+refs.button.disabled = true;
+
+refs.input.addEventListener('click', () => {
+    dateTimeWidget;
+});
+refs.button.addEventListener('click',onClickBtnStart);
+
+function onClickBtnStart() {
+};
+
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours
+//   const hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+//   return { days, hours, minutes, seconds };
+// }
+
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+const dateTimeWidget = flatpickr('#datetime-picker', {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        if (selectedDates[0] <= Date.now()) {
+            Notify.failure('Please choose a date in the future');
+            refs.button.disabled = true;
+        } else {
+            refs.button.disabled = false;
+        };
+    },
+});
