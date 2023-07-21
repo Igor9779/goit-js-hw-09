9d51a305bce49c874fabe1bbd57090597931b9c3
Igor9779 <darthhoit97@gmail.com>
2023-07-21 15:27:51 +0300

    promises-complete

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      };
+    }, delay);
+  });
+};
+
+const form = document.querySelector('.form');
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const { delay, step, amount } = e.target.elements;
+
+    const formData = {
+      delay: Number(delay.value),
+      step: Number(step.value),
+      amount: Number(amount.value),
+    };
+
+  for (let i = 0; i < formData.amount; i += 1) {
+    const delayValue = formData.delay + i * formData.step;
+
+    createPromise(i + 1, delayValue)
+      .then(({ position, delay }) => {
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
   }
-}
+});
